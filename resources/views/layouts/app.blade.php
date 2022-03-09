47fa5249d03d16d0637b301c21dfbf34557a6f24
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-03-09 12:15:22 -0600

    Added new label modal globally

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,6 +67,16 @@
         .task:hover .actions {
             display: flex;
         }
+
+        .swatch {
+            width: 30px;
+            height: 30px;
+            line-height: 30px;
+            border-radius: 50%;
+            text-align: center;
+            color: #fff;
+            cursor: pointer;
+        }
     </style>
 
 </head>
@@ -131,6 +141,80 @@
             </div>
         </div>
         <script>@if($errors->create->any()) $(document).ready(()=>{$("#addTask").modal("show")}) @endif</script>
+
+
+            <!-- Add label modal -->
+            <div class="modal fade" id="addLabel">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Create Label</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+
+                            <form method="post" action="{{url('labels')}}">
+                                @csrf
+                                <div class="form-group mb-2">
+                                    <label for="name" class="mb-2">Label Name</label>
+                                    <input type="text" name="name" id="name" class="form-control"
+                                           value="{{old('name')}}">
+                                    @error('name', 'create_label')
+                                    <p class="small text-danger">{{$message}}</p>
+                                    @enderror
+                                </div>
+
+                                <script>
+                                    $(document).ready(() => {
+                                        for(let i = 0; i < $(".swatch").length; i++) {
+                                            $(".swatch").eq(i).css("background", $(".swatch").eq(i).data("color"));
+                                        }
+                                        $(".swatch").on("click", function () {
+                                            $(".swatch").empty();
+                                            $(this).html("<i class=\"fas fa-check\"></i>");
+                                            $("#color").val($(this).data("color"));
+                                        })
+                                    });
+                                </script>
+
+                                <label class="mb-2">Label Color</label>
+
+                                <div class="d-flex mb-2">
+                                    <div class="swatch me-2" data-color="#b8255f"><i class="fas fa-check"></i></div>
+                                    <div class="swatch me-2" data-color="#db4035"></div>
+                                    <div class="swatch me-2" data-color="#ff9933"></div>
+                                    <div class="swatch me-2" data-color="#fad000"></div>
+                                    <div class="swatch me-2" data-color="#fad000"></div>
+                                    <div class="swatch me-2" data-color="#7ecc49"></div>
+                                    <div class="swatch me-2" data-color="#299438"></div>
+                                    <div class="swatch me-2" data-color="#6accbc"></div>
+                                    <div class="swatch me-2" data-color="#158fad"></div>
+                                    <div class="swatch me-2" data-color="#14aaf5"></div>
+                                </div>
+                                <div class="d-flex mb-4">
+                                    <div class="swatch me-2" data-color="#96c3eb"></div>
+                                    <div class="swatch me-2" data-color="#4073ff"></div>
+                                    <div class="swatch me-2" data-color="#884dff"></div>
+                                    <div class="swatch me-2" data-color="#af38eb"></div>
+                                    <div class="swatch me-2" data-color="#eb96eb"></div>
+                                    <div class="swatch me-2" data-color="#e05194"></div>
+                                    <div class="swatch me-2" data-color="#ff8d85"></div>
+                                    <div class="swatch me-2" data-color="#888888"></div>
+                                    <div class="swatch me-2" data-color="#b8b8b8"></div>
+                                    <div class="swatch me-2" data-color="#ccac93"></div>
+                                </div>
+
+                                <input type="hidden" name="color" id="color" value="#b8255f">
+
+                                <div class="modal-footer">
+                                    <button type="submit" class="btn bg-s_theme text-white">Add Task</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <script>@if($errors->create_label->any()) $(document).ready(()=>{$("#addLabel").modal("show")}) @endif</script>
     @endauth
 
     <main id="content" class="{{Auth::check() ? 'app-content pt-5 mt-5 bg-white' : ''}}">
