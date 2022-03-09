821be77cff5b30770ececf2ba36ecba5dc634364
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-03-09 13:36:50 -0600

    Added relationship with tasks table

--- app/Models/Label.php
+++ app/Models/Label.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Label extends Model
 {
 
+    public function tasks()
+    {
+        return $this->hasMany(Task::class, 'label_id', 'id');
+    }
+
     protected $fillable = [
         'name',
         'color',
