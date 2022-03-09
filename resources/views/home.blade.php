d129018930ab2524bc47693b33d834c4c8bf3063
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-03-09 12:13:17 -0600

    Added "Add task" button underneath task list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,16 @@
 @section('content')
 
     <div class="container w-75 mx-auto">
-        <h1 class="mb-3 fw-bolder h3">
-            Inbox
+        <h1 class="fw-bolder h3">
+            <i class="fas fa-inbox me-2"></i> Inbox
         </h1>
         <p>What will you accomplish today?</p>
 
         @foreach ($tasks as $task)
             <x-task :task="$task"></x-task>
         @endforeach
+
+        <a href="#" data-bs-toggle="modal" data-bs-target="#addTask"
+           class="btn btn-link text-s_theme text-decoration-none px-0 py-3"><i class="fas fa-plus-circle me-2"></i> Add task</a>
 
 @endsection
