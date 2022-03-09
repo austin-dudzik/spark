75cb42c7dcbcc839b0b1f63233447bbe360b563d
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-03-09 13:36:35 -0600

    Replaced broken tooltip function

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,9 +17,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
     <script>
-        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-            return new bootstrap.Tooltip(tooltipTriggerEl)
+        $(() => {
+            $('[data-bs-toggle="tooltip"]').tooltip();
         })
     </script>
 
