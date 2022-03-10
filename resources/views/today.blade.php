309ea1469ac4731bb6edb5980559721ea52694dd
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-03-10 10:34:07 -0600

    Added overdue tasks to today view

--- resources/views/today.blade.php
+++ resources/views/today.blade.php
@@ -10,11 +10,29 @@
         </h1>
         <p>What will you accomplish today?</p>
 
-        @foreach ($tasks as $task)
+        @if(count($overdue))
+        <div class="py-3">
+            <h5><i class="fas fa-exclamation-circle text-danger me-1"></i> Overdue</h5>
+            @foreach($overdue as $overdue_task)
+                <x-task :task="$overdue_task"></x-task>
+            @endforeach
+        </div>
+            <h5>Today &centerdot; {{date('M j')}}</h5>
+        @endif
+
+        @forelse ($tasks as $task)
             <x-task :task="$task"></x-task>
-        @endforeach
+        @empty
+            <div class="border-bottom"><p class="fw-600">Hooray! Looks like you're all caught up for today.</p></div>
+        @endforelse
 
         <a href="#" data-bs-toggle="modal" data-bs-target="#newTask"
-           class="btn btn-link text-s_theme text-decoration-none px-0 py-3"><i class="fas fa-plus-circle me-2"></i> Add task</a>
+           class="btn btn-link text-s_theme text-decoration-none px-0 py-3 addToday"><i class="fas fa-plus me-2"></i> Add task</a>
+
+        <script>
+            $(document).on("click", ".addToday", function() {
+                $("#due_date").val('{{\Carbon\Carbon::now()->format('Y-m-d\TH:i')}}');
+            });
+        </script>
 
 @endsection
