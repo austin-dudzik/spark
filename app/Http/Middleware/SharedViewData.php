ed480980aacc95342cb53dc4a3695450a31961bf
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-03-09 18:04:12 -0600

    Added inboxTasks variable for global use

--- app/Http/Middleware/SharedViewData.php
+++ app/Http/Middleware/SharedViewData.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Label;
 use App\Models\Task;
 use Carbon\Carbon;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 class SharedViewData
 {
@@ -21,9 +21,12 @@ class SharedViewData
     public function handle(Request $request, Closure $next)
     {
         if (auth()->check()) {
-            view()->share('labels', DB::table('labels')->where('user_id', '=', Auth::user()->id)->get());
+            view()->share('inboxTasks', Task::query()->where('tasks.user_id', '=', Auth::user()->id)->
+            whereNull('tasks.completed')->get());
+            view()->share('labels', Label::query()->with(['tasks'])->where('user_id', '=', Auth::user()->id)->get());
             view()->share('tasksToday', Task::query()->whereDate('completed', Carbon::today())->where('user_id', '=', Auth::user()->id)->get());
         }
+
 
         return $next($request);
     }
