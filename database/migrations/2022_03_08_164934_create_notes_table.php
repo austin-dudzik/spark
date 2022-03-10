4f46c4d00f3511e13af249de59e4a34425e73c8b
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-03-09 23:31:05 -0600

    Replaced column in notes migration

--- database/migrations/2022_03_08_164934_create_notes_table.php
+++ database/migrations/2022_03_08_164934_create_notes_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id');
-            $table->string('name');
+            $table->string('color');
             $table->string('content');
             $table->timestamps();
         });
