3a347c3efa485e0de38b16e42050cdeaec32d72e
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-03-09 23:33:41 -0600

    Added modals and conditional phrasing

--- resources/views/labels.blade.php
+++ resources/views/labels.blade.php
@@ -9,15 +9,19 @@
             Labels
         </h1>
         <p>Stay organized and prepared with labels, allowing you to categorize your tasks with ease.</p>
-        @forelse($labels as $label)
+        @forelse ($labels as $label)
             <div class="card border-0 rounded-0 border-bottom bg-transparent task">
                 <div class="card-body">
                     <div class="d-flex">
+                        <a href="{{url('/labels/'.$label->id)}}" class="text-dark text-decoration-none">
+                            <div class="d-flex">
                         <i class="fas fa-tag fa-flip-horizontal fa-2x me-3" style="color:{{$label->color}}"></i>
-                        <div>
+                        <span>
                             <h5 class="mb-1">{{$label->name}}</h5>
-                            <p class="small text-muted mb-2">{{count($label->tasks)}} uncompleted task</p>
-                        </div>
+                            <p class="small text-muted mb-2">{{count($label->tasks)}} uncompleted @if(count($label->tasks) === 1) task @else tasks @endif</p>
+                        </span>
+                            </div>
+                        </a>
                         <div class="ms-auto actions">
                             <button class="btn btn-link text-muted p-0 px-2" data-bs-toggle="modal"
                                     data-bs-target="#editLabelModal-{{$label->id}}">
@@ -35,6 +39,8 @@
                     </div>
                 </div>
             </div>
+            @include('modals.editLabel')
+            @include('modals.deleteLabel')
 
         @empty
             <div class="border-bottom"><p class="fw-600">No labels, yet. Create one below!</p></div>
@@ -44,7 +50,5 @@
            class="btn btn-link text-s_theme text-decoration-none px-0">
             <i class="fas fa-plus-circle me-2"></i> Add label
         </a>
-
-    @include('modals.deleteLabel')
 
 @endsection
