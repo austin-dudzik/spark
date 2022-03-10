679516b09cd24829041b79ed59b2903c6ef6582f
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-03-10 10:31:11 -0600

    Replace string with longText

--- database/migrations/2022_03_08_164934_create_notes_table.php
+++ database/migrations/2022_03_08_164934_create_notes_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id');
             $table->string('color');
-            $table->string('content');
+            $table->longText('content');
             $table->timestamps();
         });
     }
