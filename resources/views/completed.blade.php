1618f4abf1b421784b8ced30e08d98c36d9ba922
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-03-09 23:37:39 -0600

    Minor styling revisions

--- resources/views/completed.blade.php
+++ resources/views/completed.blade.php
@@ -1,11 +1,13 @@
-@section('title', 'Completed Tasks')
+@section('title', 'Completed')
 
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="mb-3 h2">✅ Completed Tasks</h1>
+    <div class="w-75 mx-auto">
+    <h1 class="h3 fw-bolder"><i class="fas fa-check-circle me-2"></i> Completed</h1>
     @foreach ($tasks as $task)
         <x-task :task="$task"></x-task>
     @endforeach
+    </div>
 
 @endsection
