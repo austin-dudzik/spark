2952fa228c1c97eed5b4fee2ac4b4260f2345cb4
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-03-09 23:29:08 -0600

    Added TodayController for today view

---
+++ app/Http/Controllers/TodayController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class TodayController extends Controller
+{
+
+    public function index()
+    {
+
+        // Return today view
+        return view('today', [
+            // Grab all tasks for the current user
+            'tasks' => Task::query()->
+            with(['label'])->
+            where('tasks.user_id', '=', Auth::user()->id)->
+            whereNull('tasks.completed')->
+            whereDate('due_date', '=', Carbon::today())->
+            orderBy(DB::raw('ISNULL(due_date), due_date'), 'ASC')->
+            get(),
+        ]);
+
+
+    }
+
+}
