18fb316a4ba461062af3c8f9e4a50a221041ba2e
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-03-11 17:42:25 -0600

    Added "At a glance" section

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Label;
+use App\Models\Note;
 use App\Models\Task;
+use Carbon\Carbon;
 use Illuminate\Contracts\Support\Renderable;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
@@ -17,10 +19,35 @@ class ProfileController extends Controller
     public function index(): Renderable
     {
 
-        $completedTasks = Task::where('user_id', Auth::id())->whereNotNull('completed')->count();
+        $completedTasks = Task::query()->
+        where('user_id', Auth::id())->
+        whereNotNull('completed')->
+        count();
+
+        $overdueTasks = Task::query()->
+        where('user_id', Auth::id())->
+        where('due_date', '<', Carbon::today())->
+        whereNull('completed')->
+        count();
+
+        $totalTasks = Task::query()->
+        where('user_id', Auth::id())->
+        count();
+
+        $notes = Note::query()->
+        where('user_id', Auth::id())->
+        count();
+
+        $labels = Label::query()->
+        where('user_id', Auth::id())->
+        count();
 
         return view('profile', [
-            'completedTasks' => $completedTasks
+            'completedTasks' => $completedTasks,
+            'overdueTasks' => $overdueTasks,
+            'totalTasks' => $totalTasks,
+            'totalNotes' => $notes,
+            'totalLabels' => $labels,
         ]);
     }
 
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,33 +5,119 @@
 @section('content')
     <div class="card w-75 mx-auto">
         <div class="bg-dark h-200px rounded-top"></div>
-    <div class="card-body p-5">
-        <img src="https://gravatar.com/avatar/{{ md5(Auth::user()->email) }}?s=100" alt="" class="img-fluid rounded mb-3" style="margin-top:-100px">
-        <h1>{{ Auth::user()->name }}</h1>
-        <p>{{ Auth::user()->email }}</p>
-        <p>{{ Auth::user()->created_at->diffForHumans() }}</p>
-        <div class="row">
-            <div class="col-md-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h4>Completed Tasks</h4>
-                        {{$completedTasks}}
+        <div class="card-body p-5">
+            <div class="d-flex justify-content-center">
+                <img src="https://gravatar.com/avatar/{{ md5(auth()->user()->email) }}?s=100" alt=""
+                     class="img-fluid rounded mb-3" style="margin-top:-100px">
+            </div>
+            <h1 class="text-center">{{ auth()->user()->name }}</h1>
+            <p class="text-center">{{ auth()->user()->email }}</p>
+            <p class="text-center">Member since {{ auth()->user()->created_at->diffForHumans() }}</p>
+
+            <p class="fw-500 h5">At a glance...</p>
+
+            <div class="row">
+
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <i class="fas fa-check-circle me-4 fa-3x text-s_theme my-auto"></i>
+                                <div>
+                                    <p class="mb-1">Completed</p>
+                                    <h3>{{$completedTasks}} @if($completedTasks === 1) task @else tasks @endif</h3>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <form method="post" action="{{url('profile/update')}}">
-                        @csrf
-                        @method('put')
-                        <div class="form-group mb-2">
-                            <label for="title">Title</label>
-                            <input type="text" name="title" id="title" class="form-control"
-                                   value="">
-                        </div>
-                        <div class="modal-footer">
-                            <button type="submit" class="btn btn-primary">Update Task</button>
-                        </div>
-                    </form>
                 </div>
+
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <i class="fas fa-exclamation-circle me-4 fa-3x text-s_theme my-auto"></i>
+                                <div>
+                                    <p class="mb-1">Overdue</p>
+                                    <h3>{{$overdueTasks}} @if($overdueTasks === 1) task @else tasks @endif</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <i class="fas fa-tasks me-4 fa-3x text-s_theme my-auto"></i>
+                                <div>
+                                    <p class="mb-1">Total</p>
+                                    <h3>{{$totalTasks}} @if($totalTasks === 1) task @else tasks @endif</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <i class="fas fa-notes me-4 fa-3x text-s_theme my-auto"></i>
+                                <div>
+                                    <p class="mb-1">Notes</p>
+                                    <h3>{{$totalNotes}} total</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <i class="fas fa-tags fa-flip-horizontal me-4 fa-3x text-s_theme my-auto"></i>
+                                <div>
+                                    <p class="mb-1">Labels</p>
+                                    <h3>{{$totalLabels}} total</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <i class="fas fa-calendar-day me-4 fa-3x text-s_theme my-auto"></i>
+                                <div>
+                                    <p class="mb-1">Daily goal</p>
+                                    <h3>{{auth()->user()->daily_goal}} @if(auth()->user()->daily_goal === 1) task @else tasks @endif</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <i class="fas fa-calendar-week me-4 fa-3x text-s_theme my-auto"></i>
+                                <div>
+                                    <p class="mb-1">Weekly goal</p>
+                                    <h3>{{auth()->user()->weekly_goal}} @if(auth()->user()->weekly_goal === 1) task @else tasks @endif</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
         </div>
-    </div>
     </div>
 @endsection
