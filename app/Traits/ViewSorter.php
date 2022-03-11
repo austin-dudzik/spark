c918e26f9d1ab51f971c559a83ac0ed594f3c64d
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-03-11 16:42:30 -0600

    Added ViewSorter trait and views table migration

---
+++ app/Traits/ViewSorter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\View;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+trait ViewSorter
+{
+
+    protected function getSorters() {
+
+        // Get the user view
+        $view = View::query()->
+        where('user_id', '=', Auth::user()->id)->
+        first();
+
+        // Determine column sort
+        $view->sort_by = match ($view->sort_by) {
+            'alphabetically' => 'title',
+            'date_added' => 'created_at',
+            'date_updated' => 'updated_at',
+            default => DB::raw('ISNULL(due_date), due_date'),
+        };
+
+        return $view;
+
+    }
+}
---
+++ database/migrations/2022_03_11_105641_create_views_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('views', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('sort_by')->default('due_date');
+            $table->string('order_by')->default('asc');
+            $table->string('task_view')->default('list');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('views');
+    }
+};
