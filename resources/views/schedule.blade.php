35ce9c876546b14429c5ca22221a779778ab5089
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-03-11 22:03:28 -0600

    Added loader and fixed button color

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -7,14 +7,15 @@
 
     <div class="w-75 mx-auto">
         <h3 class="mb-3"><i class="far fa-list me-1"></i> Schedule</h3>
+        <p>A breakdown of what you've got going on now and coming up.</p>
 
         @if(count($overdue))
-        <div class="py-3">
-            <h5><i class="fas fa-exclamation-circle text-danger me-1"></i> Overdue</h5>
-            @foreach($overdue as $overdue_task)
-                <x-task :task="$overdue_task"></x-task>
-            @endforeach
-        </div>
+            <div class="py-3">
+                <h5><i class="fas fa-exclamation-circle text-danger me-1"></i> Overdue</h5>
+                @foreach($overdue as $overdue_task)
+                    <x-task :task="$overdue_task"></x-task>
+                @endforeach
+            </div>
         @endif
 
         <div id="schedule-list">
@@ -22,7 +23,7 @@
         </div>
 
         <div class="d-flex justify-content-center">
-            <button class="loadMore btn btn-primary px-5"><i class="far fa-long-arrow-down me-2"></i> Load more</button>
+            <button class="loadMore btn bg-s_theme text-white px-5"><i class="far fa-long-arrow-down me-2"></i> Load more</button>
         </div>
 
     </div>
@@ -35,6 +36,7 @@
     <script>
         let page = 1;
         $(".loadMore").on("click", function () {
+            $(this).addClass("disabled").html('<i class="fas fa-spinner-third mx-4 fa-spin"></i>');
             page++;
             $.ajax({
                 url: "/schedule/",
@@ -44,7 +46,7 @@
                 type: "GET",
                 return: "html",
                 success: function (result) {
-                    console.log(result);
+                    $(".loadMore").removeClass("disabled").html('<i class="far fa-long-arrow-down me-2"></i> Load more');
                     $("#schedule-list").append(result);
                 }
             });
